2a170ebe512774a90c5f24920f3990695f81721b
Connor Braa <connor@dagger.io>
2025-04-02 16:41:10 -0700

    working 3rd party gsearch MCP example
    Signed-off-by: Connor Braa <connor@dagger.io>

--- mcp-caller/main.go
+++ mcp-caller/main.go
@@ -1,37 +1,35 @@
 // A generated module for McpCaller functions
-//
-// This module has been generated via dagger init and serves as a reference to
-// basic module structure as you get started with Dagger.
-//
-// Two functions have been pre-created. You can modify, delete, or add to them,
-// as needed. They demonstrate usage of arguments and return types using simple
-// echo and grep commands. The functions can be called from the dagger CLI or
-// from one of the SDKs.
-//
-// The first line in this comment block is a short description line and the
-// rest is a long description with more detail on the module's purpose or usage,
-// if appropriate. All modules should have a short description.
 
 package main
 
 import (
-	"context"
 	"dagger/mcp-caller/internal/dagger"
 )
 
 type McpCaller struct{}
 
-// Returns a container that echoes whatever string argument is provided
-func (m *McpCaller) ContainerEcho(stringArg string) *dagger.Container {
-	return dag.Container().From("alpine:latest").WithExec([]string{"echo", stringArg})
+func (m *McpCaller) WolfiNode() *dagger.Container {
+	return dag.Wolfi().Container(dagger.WolfiContainerOpts{
+		Packages: []string{"nodejs", "npm"},
+	})
 }
 
-// Returns lines that match a pattern in the files of the provided Directory
-func (m *McpCaller) GrepDir(ctx context.Context, directoryArg *dagger.Directory, pattern string) (string, error) {
-	return dag.Container().
-		From("alpine:latest").
-		WithMountedDirectory("/mnt", directoryArg).
-		WithWorkdir("/mnt").
-		WithExec([]string{"grep", "-R", pattern, "."}).
-		Stdout(ctx)
+func (m *McpCaller) Playwright() *dagger.Container {
+	return dag.Container().From("mcr.microsoft.com/playwright:v1.51.1-noble")
+}
+
+func (m *McpCaller) MCPGSearch() *dagger.Container {
+	return m.Playwright().
+		WithEntrypoint([]string{"npx", "-y", "g-search-mcp"})
+}
+
+func (m *McpCaller) MCPAWS() *dagger.Container {
+	return dag.Container().From("ghcr.io/alexei-led/aws-mcp-server:latest")
+}
+
+func (m *McpCaller) LlmWithMCP() *dagger.LLM {
+	return dag.LLM().
+		WithMCP(m.MCPGSearch()).
+		// WithMCP(m.MCPAWS()).
+		WithPrompt("list your available tools")
 }
